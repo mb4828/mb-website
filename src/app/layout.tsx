b240a3e6ac6ccb54df5e339750b71166b6b83184
Matt Brauner <mattbrauner44@gmail.com>
2023-09-16 02:05:24 -0400

    Cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,21 @@
-import "./globals.scss";
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
+import './globals.scss';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
 
 // font awesome
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
-const opensans = Open_Sans({ subsets: ["latin"] });
+const opensans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Matt Brauner | Senior Software Engineer, New York, NY",
+  title: 'Matt Brauner | Senior Software Engineer, New York, NY',
   description:
-    "Accomplished technologist with professional experience in full-stack web development, financial technology, and data analysis",
+    'Accomplished technologist with professional experience in full-stack web development, financial technology, and data analysis',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={opensans.className}>{children}</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
-import { Libre_Baskerville } from "next/font/google";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from 'next/image';
+import styles from './page.module.scss';
+import { Libre_Baskerville } from 'next/font/google';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFacebookSquare,
   faGithubSquare,
   faInstagramSquare,
   faLinkedin,
   faSquareThreads,
-} from "@fortawesome/free-brands-svg-icons";
-import Link from "next/link";
-import bk from "../../public/matt_bk.jpg";
+} from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
+import bk from '../../public/matt_bk.jpg';
 
 const libre_baskerville = Libre_Baskerville({
-  weight: "400",
-  subsets: ["latin"],
+  weight: '400',
+  subsets: ['latin'],
 });
 
 const today = new Date();
@@ -22,19 +22,10 @@ const today = new Date();
 export default function Home() {
   return (
     <>
-      <header
-        className={styles.header_wrapper}
-        style={{ backgroundImage: `url(${bk.src})` }}
-      >
+      <header className={styles.header_wrapper} style={{ backgroundImage: `url(${bk.src})` }}>
         <div className={styles.header}>
           <Image src="monogram_white.png" width={40} height={40} alt="" />
-          <h1
-            className={
-              libre_baskerville.className + " animate__animated animate__bounce"
-            }
-          >
-            Matt Brauner
-          </h1>
+          <h1 className={libre_baskerville.className}>Matt Brauner</h1>
           <h2>
             Senior Software Engineer
             <br />
@@ -75,100 +66,39 @@ export default function Home() {
 
           <div>
             <h2>Skills</h2>
-            <p>
-              Full-stack web development, financial technology, and data
-              analysis
-            </p>
+            <p>Full-stack web development, financial technology, and data analysis</p>
           </div>
 
           <div>
             <h2>Interests</h2>
-            <p>
-              Clarinetist with a passion for art, science, technology, and
-              aviation
-            </p>
+            <p>Clarinetist with a passion for art, science, technology, and aviation</p>
           </div>
         </div>
-
-        {/* <style type="text/css">
-.s-wrapper {
-display: flex;
-justify-content:center;
-line-height:40px;
-gap: 28px;
-}
-a, a:hover, a:focus, a:visited {
-color: rgb(94, 94, 94);
-transition: color .25s ease-in-out;
-}
-a.fb:hover {
-color: #0165E1
-}
-a.ig:hover {
-color: #C13584;
-}
-a.thr:hover {
-color: #000000;
-}
-a.li:hover {
-color: #0A66C2;
-}
-a.gh:hover {
-color:#000000;
-}
-</style> */}
       </main>
 
       <div className={styles.social_links}>
-        <Link
-          className="fb"
-          href="http://facebook.com/matt.brauner/"
-          target="_blank"
-          rel="noopener"
-        >
+        <Link className="fb" href="http://facebook.com/matt.brauner/" target="_blank" rel="noopener">
           <FontAwesomeIcon icon={faFacebookSquare} size="2xl" />
         </Link>
 
-        <Link
-          className="ig"
-          href="http://instagram.com/matt.brauner/"
-          target="_blank"
-          rel="noopener"
-        >
+        <Link className="ig" href="http://instagram.com/matt.brauner/" target="_blank" rel="noopener">
           <FontAwesomeIcon icon={faInstagramSquare} size="2xl" />
         </Link>
 
-        <Link
-          className="thr"
-          href="https://www.threads.net/@matt.brauner"
-          target="_blank"
-          rel="noopener"
-        >
+        <Link className="thr" href="https://www.threads.net/@matt.brauner" target="_blank" rel="noopener">
           <FontAwesomeIcon icon={faSquareThreads} size="2xl" />
         </Link>
 
-        <Link
-          className="li"
-          href="http://linkedin.com/in/matt-brauner/"
-          target="_blank"
-          rel="noopener"
-        >
+        <Link className="li" href="http://linkedin.com/in/matt-brauner/" target="_blank" rel="noopener">
           <FontAwesomeIcon icon={faLinkedin} size="2xl" />
         </Link>
 
-        <Link
-          href="http://github.com/mb4828"
-          className="gh"
-          target="_blank"
-          rel="noopener"
-        >
+        <Link href="http://github.com/mb4828" className="gh" target="_blank" rel="noopener">
           <FontAwesomeIcon icon={faGithubSquare} size="2xl" />
         </Link>
       </div>
 
-      <footer className={styles.footer}>
-        Copyright &copy; {today.getFullYear()} Matt Brauner
-      </footer>
+      <footer className={styles.footer}>Copyright &copy; {today.getFullYear()} Matt Brauner</footer>
     </>
   );
 }
